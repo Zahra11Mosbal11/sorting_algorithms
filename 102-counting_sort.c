c511bf023e962eb16456e9530a53a20883564896
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-01-23 00:23:55 -0800

    counting_sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,55 @@
+#include "sort.h"
+/**
+ * _bigN -  Give me the largest number in an array of integers
+ * @array: The Int array
+ * @size: Size of array
+ * Return: The largest number
+ */
+int _bigN(int *array, size_t size)
+{
+	int k = 0;
+
+	while (size--)
+	{
+		if (k < array[size])
+			k = array[size];
+	}
+	return (k);
+}
+/**
+ * counting_sort - sort an array with the counting sort algorithm
+ * @array: The Int array
+ * @size: Size of array
+ * Return: The sorted list
+ */
+void counting_sort(int *array, size_t size)
+{
+	size_t i;
+	int k = _bigN(array, size);
+	int *aux = malloc((k + 1) * sizeof(int));
+	int *array_sort = malloc(size * sizeof(int));
+
+	if (!aux || !array_sort)
+	{
+		free(aux);
+		free(array_sort);
+		return;
+	}
+	for (i = 0; (int)i < k + 1; i++)
+		aux[i] = 0;
+
+	for (i = 0; i < size; i++)
+		aux[array[i]]++;
+	for (i = 1; (int)i < k + 1; i++)
+		aux[i] += aux[i - 1];
+	print_array(aux, k + 1);
+	for (i = 0; i < size; i++)
+	{
+		array_sort[aux[array[i]] - 1] = array[i];
+		aux[array[i]]--;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = array_sort[i];
+	free(aux);
+	free(array_sort);
+}
--- sort.h
+++ sort.h
@@ -33,4 +33,6 @@ void quick_sort_lim(int *array, int start, int end, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+int _bigN(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 #endif
